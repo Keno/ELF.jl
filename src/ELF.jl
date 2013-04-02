e1f9b49440f14333d627df02e0ddfd087ff3ec59
Keno Fischer <kfischer@college.harvard.edu>
2013-04-02 02:35:06 -0400

    Inital Feature for working with ELF files
    Works fine for loading sys161 kernel files into memory, but should be
    very useful in general

---
+++ src/ELF.jl
@@ -0,0 +1,244 @@
+module ELF
+    include("constants.jl")
+    using StrPack
+    import Base.read
+
+    abstract ELFHeader
+    abstract ELFSectionHeader
+    abstract ELFSymtabEntry
+    abstract ELFRel
+    abstract ELFRela
+    abstract ELFProgramHeader
+    abstract ELFFile
+
+    module ELF32
+        import ELF
+        using StrPack
+        @struct immutable Header <: ELF.ELFHeader
+            e_type::Uint16
+            e_machine::Uint16
+            e_version::Uint32
+            e_entry::Uint32
+            e_phoff::Uint32
+            e_shoff::Uint32
+            e_flags::Uint32
+            e_ehsize::Uint16
+            e_phentsize::Uint16
+            e_phnum::Uint16
+            e_shentsize::Uint16
+            e_shnum::Uint16
+            e_shstrndx::Uint16
+        end
+
+        @struct immutable SectionHeader <: ELF.ELFSectionHeader
+            sh_name::Uint32
+            sh_type::Uint32
+            sh_flags::Uint32
+            sh_addr::Uint32
+            sh_offset::Uint32
+            sh_size::Uint32
+            sh_link::Uint32
+            sh_info::Uint32
+            sh_addralign::Uint32
+            sh_entsize::Uint32
+        end
+
+        @struct immutable SymtabEntry <: ELF.ELFSymtabEntry
+            st_name::Uint32
+            st_value::Uint32
+            st_size::Uint32
+            st_info::Uint8
+            st_other::Uint8
+            st_shndx::Uint16
+        end
+
+        @struct immutable Rel <: ELF.ELFRel
+            r_offset::Uint32
+            r_info::Uint32
+        end
+
+        @struct immutable Rela <: ELF.ELFRel
+            r_offset::Uint32
+            r_info::Uint32
+            r_addend::Int32
+        end
+
+        @struct immutable ProgramHeader <: ELF.ELFProgramHeader
+            p_type::Uint32
+            p_offset::Uint32
+            p_vaddr::Uint32
+            p_paddr::Uint32
+            p_filesz::Uint32
+            p_memsz::Uint32
+            p_flags::Uint32
+            p_align::Uint32
+        end
+
+        type File <: ELF.ELFFile
+            endianness::Symbol
+            ei_version::Uint8
+            ei_osabi::Uint8
+            header::Header
+            pheaders::Array{ProgramHeader,1}
+            sheaders::Array{SectionHeader,1}
+        end
+    end
+
+    module ELF64
+        import ELF
+        using StrPack
+        @struct immutable Header <: ELF.ELFHeader
+            e_type::Uint16
+            e_machine::Uint16
+            e_version::Uint32
+            e_entry::Uint64
+            e_phoff::Uint64
+            e_shoff::Uint64
+            e_flags::Uint32
+            e_ehsize::Uint16
+            e_phentsize::Uint16
+            e_phnum::Uint16
+            e_shentsize::Uint16
+            e_shnum::Uint16
+            e_shstrndx::Uint16
+        end
+
+        @struct immutable SectionHeader <: ELF.ELFSectionHeader
+            sh_name::Uint32
+            sh_type::Uint32
+            sh_flags::Uint64
+            sh_addr::Uint64
+            sh_offset::Uint64
+            sh_size::Uint64
+            sh_link::Uint32
+            sh_info::Uint32
+            sh_addralign::Uint64
+            sh_entsize::Uint64
+        end
+
+        @struct immutable SymtabEntry <: ELF.ELFSymtabEntry
+            st_name::Uint32
+            st_info::Uint8
+            st_other::Uint8
+            st_shndx::Uint16
+            st_value::Uint32
+            st_size::Uint64
+        end
+
+        @struct immutable Rel <: ELF.ELFRel
+            r_offset::Uint64
+            r_info::Uint64
+        end
+
+        @struct immutable Rela <: ELF.ELFRel
+            r_offset::Uint64
+            r_info::Uint64
+            r_addend::Int64
+        end
+
+        @struct immutable ProgramHeader <: ELF.ELFProgramHeader
+            p_type::Uint32
+            p_flags::Uint32
+            p_offset::Uint64
+            p_vaddr::Uint64
+            p_paddr::Uint64
+            p_filesz::Uint64
+            p_memsz::Uint64
+            p_align::Uint64
+        end
+
+        type File <: ELF.ELFFile
+            endianness::Symbol
+            ei_version::Uint8
+            ei_osabi::Uint8
+            header::Header
+            pheaders::Array{ProgramHeader,1}
+            sheaders::Array{SectionHeader,1}
+        end
+    end
+
+    function endianness(ei_data::Uint8) 
+        if ei_data == ELFDATA2MSB
+            :BigEndian
+        elseif ei_data == ELFDATA2LSB
+            :LittleEndian
+        else
+            error("Invalid Data Specification")
+        end
+    end
+
+    function readmeta(io::IO)
+        mag0 = read(io,Uint8)
+        mag1 = read(io,Uint8)
+        mag2 = read(io,Uint8)
+        mag3 = read(io,Uint8)
+        if((mag0 != '\177') || (mag1 != 'E') || (mag2 != 'L') || (mag3 != 'F'))
+            error("Magic Number does not match")
+        end
+        class = read(io,Uint8)
+        data = read(io,Uint8)
+        version = read(io,Uint8)
+        osabi = read(io,Uint8)
+        abiversion = read(io,Uint8)
+        skip(io,7)
+        if class == ELFCLASS32
+            header = unpack(io,ELF32.Header,endianness(data))
+            return ELF32.File(endianness(data),version,osabi,header,Array(ELF32.ProgramHeader,header.e_phnum),
+                                                                    Array(ELF32.SectionHeader,header.e_shnum))
+        elseif class == ELFCLASS64
+            header = unpack(io,ELF64.Header,endianness(data))
+            return ELF64.File(endianness(data),version,osabi,header,Array(ELF64.ProgramHeader,header.e_phnum),
+                                                                    Array(ELF64.SectionHeader,header.e_shnum))
+        else 
+            error("Invalid File Class")
+        end
+    end 
+
+    function read(io::IO,::Type{ELFProgramHeader},f::ELFFile)
+        s = StrPack.calcsize(eltype(f.pheaders))
+        if s > f.header.e_phentsize
+            error("Missing data for program header")
+        end 
+        ret = unpack(io,eltype(f.pheaders),f.endianness)
+        skip(io,f.header.e_phentsize-s)
+        ret
+    end
+
+    function read(io::IO,::Type{ELFSectionHeader},f::ELFFile)
+        s = StrPack.calcsize(eltype(f.sheaders))
+        if s > f.header.e_shentsize
+            error("Missing data for program header")
+        end 
+        ret = unpack(io,eltype(f.sheaders),f.endianness)
+        skip(io,f.header.e_shentsize-s)
+        ret
+    end
+
+    function readheaders(io::IO)
+        file = readmeta(io)
+        if file.header.e_phnum>0
+            seek(io,file.header.e_phoff)
+            for i = 1:file.header.e_phnum
+                file.pheaders[i] = read(io,ELFProgramHeader,file);
+            end
+        end
+        if file.header.e_shnum>0
+            seek(io,file.header.e_shoff)
+            for i = 1:file.header.e_shnum
+                file.sheaders[i] = read(io,ELFSectionHeader,file);
+            end
+        end
+        file
+    end
+
+    function read(io::IO,x::Array{Uint8,1},file::ELFFile,header::ELFProgramHeader)
+        seek(io,header.p_offset)
+        read(io,x)
+    end
+
+    function read(io::IO,file::ELFFile,header::ELFProgramHeader)
+        x = Array(Uint8,header.p_filesz)
+        read(io,x,file,header)
+        x
+    end
+end
